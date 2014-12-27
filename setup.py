8427bbd7b94e95284b13479bfe454d4d6681e1a6
Alexander Artemenko <svetlyak.40wt@gmail.com>
2014-12-27 14:03:49 +0300

    Add simple setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='unittest-assertions',
+      version='0.1.0',
+      description='A set of helpers for unittest assertions and checks.',
+      author='Alexander Artemenko',
+      author_email='svetlyak.40wt@gmail.com')
